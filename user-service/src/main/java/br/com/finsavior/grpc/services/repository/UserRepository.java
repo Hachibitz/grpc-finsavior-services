6d58906462d0d55aaaeb94e06a045a1e724a897c
Hachibitz <felipealmeida.ns@outlook.com>
2023-10-01 18:33:43 -0300

    feat: adding delete account service

--- user-service/src/main/java/br/com/finsavior/grpc/services/repository/UserRepository.java
+++ user-service/src/main/java/br/com/finsavior/grpc/services/repository/UserRepository.java
@@ -2,7 +2,13 @@ package br.com.finsavior.grpc.services.repository;
 
 import br.com.finsavior.grpc.services.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    @Modifying
+    @Query(nativeQuery = true, value = "UPDATE users SET enabled = 0, del_fg = 'S' WHERE id = ?1")
+    public void deleteUser(Long id);
 }
--- user-service/src/main/java/br/com/finsavior/grpc/services/service/UserService.java
+++ user-service/src/main/java/br/com/finsavior/grpc/services/service/UserService.java
@@ -4,23 +4,22 @@ import br.com.finsavior.grpc.services.model.entity.Role;
 import br.com.finsavior.grpc.services.model.entity.User;
 import br.com.finsavior.grpc.services.model.enums.UserRoleEnum;
 import br.com.finsavior.grpc.services.repository.UserRepository;
-import br.com.finsavior.grpc.user.SignUpRequest;
-import br.com.finsavior.grpc.user.SignUpResponse;
-import br.com.finsavior.grpc.user.UserServiceGrpc;
+import br.com.finsavior.grpc.user.*;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.modelmapper.ModelMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.transaction.Transactional;
+import java.util.Optional;
+
 @GrpcService
+@Slf4j
 public class UserService extends UserServiceGrpc.UserServiceImplBase {
 
     @Autowired
     UserRepository userRepository;
-
-    Logger logger = LoggerFactory.getLogger(UserServiceGrpc.UserServiceImplBase.class);
 
     @Override
     public void signUp(SignUpRequest request, StreamObserver<SignUpResponse> responseObserver) {
@@ -34,9 +33,31 @@ public class UserService extends UserServiceGrpc.UserServiceImplBase {
                 .setMessage("Cadastro realizado com sucesso!")
                 .build();
 
-        logger.info("Cadastro realizado com sucesso!");
+        log.info("Cadastro realizado com sucesso: "+user.getUsername());
 
         responseObserver.onNext(signUpResponse);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    @Transactional
+    public void deleteAccount(DeleteAccountRequest request, StreamObserver<DeleteAccountResponse> responseObserver) {
+        Optional<User> user = Optional.ofNullable(userRepository.findByUsername(request.getUsername()));
+
+        if(user.isPresent()) {
+            userRepository.deleteUser(user.get().getId());
+
+            DeleteAccountResponse response = DeleteAccountResponse.newBuilder()
+                    .setMessage("Account deleted successfully.")
+                    .build();
+
+            log.info("Account deleted successfully: "+user.get().getUsername());
+
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } else {
+            Throwable throwable = new Throwable("Usuário não encontrado.");
+            responseObserver.onError(throwable);
+        }
     }
 }
